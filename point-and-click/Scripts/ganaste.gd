84dd784632b8249794da9d1c7a5a30d6f1d136a4
RoCso84 <40866824+RoCso84@users.noreply.github.com>
2025-11-30 02:26:40 -0300

    Revert "Rodri/pacha"

--- point-and-click/Scripts/ganaste.gd
+++ point-and-click/Scripts/ganaste.gd
@@ -1,31 +1,11 @@
 extends Node2D
 
-@onready var labels = [$TiempoLabel,$PuntajeLabel,$VidasLabel]
-
-
 func _ready():
 	var t = TimerData.tiempo_total
 	var s = TimerData.score
 	var v = TimerData.lives
-	
-	$TiempoLabel.text= "Tiempo Total: %d" % t
-	$PuntajeLabel.text = "Puntaje Final: %d" % s
-	$VidasLabel.text = "Vidas restantes: %d" % v
-	
-	fade_labels_sequentials(labels,1.0)	
-	#$Label.text = "Ganaste!\nTiempo: %.2f s\nPuntaje: %.2f\nVidas Restantes: %.2f" % [t,s,v]
+	$Label.text = "Ganaste!\nTiempo: %.2f s\nPuntaje: %.2f\nVidas Restantes: %.2f" % [t,s,v]
 	$Button_Volver.pressed.connect(_on_volver_pressed)
-
-func fade_labels_sequentials(label_list,fade_time: float):
-	for l in label_list:
-		l.modulate.a = 0.0
-		start_fade_sequence(label_list,fade_time)
-
-func start_fade_sequence(label_list,fade_time) ->void:
-	for lbl in label_list:
-		var tween = get_tree().create_tween()
-		tween.tween_property(lbl,"modulate:a",1.0, fade_time)
-		await tween.finished
 
 func _on_volver_pressed():
 	get_tree().change_scene_to_file("res://Escenas/MenuInicial.tscn")
--- point-and-click/Scripts/menu_inicial.gd
+++ point-and-click/Scripts/menu_inicial.gd
@@ -1,8 +1,8 @@
 extends Control
-
-@onready var labels = [$Historia1,$Historia2,$Historia3]
-
-
+func _ready():
+	$Fondo/VBoxContainer/JUGAR.pressed.connect(_on_iniciar_pressed)
+	$Fondo/VBoxContainer/CREDITOS.pressed.connect(_on_creditos_pressed)
+	$Fondo/VBoxContainer/SALIR.pressed.connect(_on_salir_pressed)
 
 func _on_iniciar_pressed():
 	get_tree().change_scene_to_file("res://Escenas/gameplay.tscn")
@@ -12,42 +12,3 @@ func _on_creditos_pressed():
 
 func _on_salir_pressed():
 	get_tree().quit()
-
-
-
-func _ready():
-	$FondoPlaceholder/VBoxContainer/JUGAR.pressed.connect(_on_iniciar_pressed)
-	$FondoPlaceholder/VBoxContainer/CREDITOS.pressed.connect(_on_creditos_pressed)
-	$FondoPlaceholder/VBoxContainer/SALIR.pressed.connect(_on_salir_pressed)
-
-
-#---------------------------------------------------------------------------------------
-	var t ="En las sombras del viejo teatro, el Gran Mago ha perdido sus objetos más preciados.
-Sin su sombrero encantado, su varita ancestral y las reliquias de su poder,
-la función no puede comenzar."
-
-	var s ="Explora cada rincón, descubre los secretos ocultos entre la escenografía
-y recupera todo lo que el teatro ha escondido."
-
-	var v ="Solo cuando tenga cada uno de sus artefactos, la magia podrá volver a brillar en el escenario.¿Estás listo para ayudarlo?"
-
-
-	$Historia1.text=  t
-	$Historia2.text =  s
-	$Historia3.text =  v
-	
-	fade_labels_sequentials(labels,2)	
-
-func fade_labels_sequentials(label_list,fade_time: float):
-	for l in label_list:
-		l.modulate.a = 0.0
-		start_fade_sequence(label_list,fade_time)
-
-func start_fade_sequence(label_list,fade_time) ->void:
-	for lbl in label_list:
-		var tween = get_tree().create_tween()
-		tween.tween_property(lbl,"modulate:a",1.0, fade_time)
-		await tween.finished
-
-
-#------------------------------------------------------------------------
