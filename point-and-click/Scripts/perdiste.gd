494b6ccad43ccd3139aaeb60b469bd6e9c3930f8
duk3nd3 <duk3nd3@gmail.com>
2025-11-30 15:48:06 -0300

    Fusion branch rodri/pacha Y Duk3nd3

--- point-and-click/Scripts/perdiste.gd
+++ point-and-click/Scripts/perdiste.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready():
-	$perder.play()
+	$Button_Volver.pressed.connect(_on_volver_pressed)
 
 func _on_volver_pressed():
 	get_tree().change_scene_to_file("res://Escenas/MenuInicial.tscn")
